693d762875a5998f7d1f4eeeb56e8aec4e4fc6da
Tim Zaman <tzaman@Tims-Mac-Pro.fritz.box>
2015-08-30 14:56:52 +0200

    Small addition

--- utils_general.h
+++ utils_general.h
@@ -56,6 +56,15 @@
 #endif
 
 
+template <typename T>
+std::string to_string_with_precision(const T a_value, const int n = 6)
+{
+    std::ostringstream out;
+    out << std::setprecision(n) << a_value;
+    return out.str();
+}
+
+
 using namespace std;
 namespace po = boost::program_options;
 namespace fs = boost::filesystem;
