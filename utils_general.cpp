a88732793a3cc752405c52dcf1f56ab393941fe9
Tim Zaman <timbobel@gmail.com>
2015-11-04 14:07:53 +0100

    Added another function

--- utils_general.cpp
+++ utils_general.cpp
@@ -3,7 +3,24 @@
 #include "utils_general.h"
 
 
-
+std::string util::urlencode(const std::string &s){
+	static const char lookup[]= "0123456789abcdef";
+	std::stringstream e;
+	for(int i=0, ix=s.length(); i<ix; i++){
+		const char& c = s[i];
+		if ( (48 <= c && c <= 57) ||//0-9
+				(65 <= c && c <= 90) ||//abc...xyz
+				(97 <= c && c <= 122) || //ABC...XYZ
+				(c=='-' || c=='_' || c=='.' || c=='~')  ) {
+			e << c;
+		} else {
+			e << '%';
+			e << lookup[ (c&0xF0)>>4 ];
+			e << lookup[ (c&0x0F) ];
+		}
+	}
+	return e.str();
+}
 
 
 double util::calcMean(std::vector<double> scores){ //calculates mean
