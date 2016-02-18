060cae76441045e470a2b9df212bb7609ac964d5
Tim Zaman <tzaman@Tims-Mac-Pro.fritz.box>
2016-02-18 17:55:16 +0100

    Minor

--- mfs.h
+++ mfs.h
@@ -10,26 +10,22 @@
 -   Author      :   Mike Johnson - Banctec AB 03/07/96
 -   Adaptations :   Tim Zaman - Picturae, Pixelprisma, 06/05/2015
 --------------------------------------------------------------------------------
-*/
-
-/*
-
-Copyright (c) 1996 Mike Johnson
-Copyright (c) 1996 BancTec AB
-
-Permission to use, copy, modify, distribute, and sell this software
-for any purpose is hereby granted without fee, provided
-that (i) the above copyright notices and this permission notice appear in
-all copies of the software and related documentation, and (ii) the names of
-Mike Johnson and BancTec may not be used in any advertising or
-publicity relating to the software.
-
-THE SOFTWARE IS PROVIDED "AS-IS" AND WITHOUT WARRANTY OF ANY KIND,
-EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
-WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
-
-Additions: Eric Bruneton, Antoine Begault, Guillaume Piolat, Tim Zaman
- 
+-   Copyright (c) 1996 Mike Johnson
+-   Copyright (c) 1996 BancTec AB
+-   
+-   Permission to use, copy, modify, distribute, and sell this software
+-   for any purpose is hereby granted without fee, provided
+-   that (i) the above copyright notices and this permission notice appear in
+-   all copies of the software and related documentation, and (ii) the names of
+-   Mike Johnson and BancTec may not be used in any advertising or
+-   publicity relating to the software.
+-   
+-   THE SOFTWARE IS PROVIDED "AS-IS" AND WITHOUT WARRANTY OF ANY KIND,
+-   EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
+-   WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
+-   
+-   Additions: Eric Bruneton, Antoine Begault, Guillaume Piolat, Tim Zaman
+--------------------------------------------------------------------------------
 */
 
 typedef struct {
--- zxingwrapper.h
+++ zxingwrapper.h
@@ -15,9 +15,7 @@ Copyright (c) 2016 Tim Zaman
 Permission to use, copy, modify, distribute, and sell this software
 for any purpose is hereby granted without fee, provided
 that (i) the above copyright notices and this permission notice appear in
-all copies of the software and related documentation, and (ii) the names of
-Mike Johnson and BancTec may not be used in any advertising or
-publicity relating to the software.
+all copies of the software and related documentation.
 
 THE SOFTWARE IS PROVIDED "AS-IS" AND WITHOUT WARRANTY OF ANY KIND,
 EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
