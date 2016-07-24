2a40eb313766dbbe67a8709e550a7935c8aec63c
Tim Zaman <timbobel@gmail.com>
2016-07-25 00:16:33 +0200

    Fix in keepalive and addition in notifier

--- keepalive.mm
+++ keepalive.mm
@@ -1,12 +1,11 @@
 /**
 --------------------------------------------------------------------------------
 -   Module      :   keepalive.mm
--   Description :   A wrapper to keep a QT app alive, written in C++ and C#,
--                   so made for the OSX platform.
+-   Description :   A wrapper to keep an app alive, written
+-                   in C++ and Objective-C so made for the OSX platform.
 -   Author      :   Tim Zaman, 18-FEB-2016
 --------------------------------------------------------------------------------
 */
-
 
 #include "keepalive.h"
 #import <IOKit/pwr_mgt/IOPMLib.h>
@@ -21,17 +20,17 @@
 IOPMAssertionID assertionID;
 
 void KeepAlive::KeepAliveMM(){
-	// NOTE: IOPMAssertionCreateWithName limits the string to 128 characters. 
-	CFStringRef reasonForActivity = CFSTR("Program Running.");
-	IOReturn success = IOPMAssertionCreateWithName(kIOPMAssertionTypeNoDisplaySleep, 
-	                                    kIOPMAssertionLevelOn, reasonForActivity, &assertionID); 
-	if (success == kIOReturnSuccess){
-		// @TODO?
-	} else {
-		// @TODO?
-	}
+    // NOTE: IOPMAssertionCreateWithName limits the string to 128 characters. 
+    CFStringRef reasonForActivity = CFSTR("Program Running.");
+    IOReturn success = IOPMAssertionCreateWithName(kIOPMAssertionTypeNoDisplaySleep, 
+                                kIOPMAssertionLevelOn, reasonForActivity, &assertionID); 
+    if (success == kIOReturnSuccess){
+        // @TODO?
+    } else {
+        // @TODO?
+    }
 }
 
 void KeepAlive::KeepAliveDestructorMM(){
-	IOReturn success = IOPMAssertionRelease(assertionID);
+    IOReturn success = IOPMAssertionRelease(assertionID);
 }
